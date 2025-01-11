2d98c6fb41b7a5d34efd49e66c84792d21c6f1c5
Edwin Park <esp1@cornell.edu>
2025-01-10 17:58:54 -0800

    remove telemere, upgrade clojure to 1.12

--- src/esp1/fsr/track_uri.clj
+++ src/esp1/fsr/track_uri.clj
@@ -1,5 +1,4 @@
-(ns esp1.fsr.track-uri
-  (:require [taoensso.telemere :as t]))
+(ns esp1.fsr.track-uri)
 
 (def ^:dynamic tracked-uris-atom
   "Dynamically bound atom to hold tracked URIs.
@@ -9,7 +8,6 @@
 (defn track-uri
   "Adds URI to `tracked-uris-atom` if it exists."
   [uri]
-  (t/log! ["track-uri:" uri])
   (when tracked-uris-atom
     (swap! tracked-uris-atom conj uri))
   uri)
